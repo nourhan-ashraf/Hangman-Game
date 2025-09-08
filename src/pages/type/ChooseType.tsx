59b412dce1628989200e54b3ca02625be340a77c
Nourhan Elsherif <66208099+nourhan-ashraf@users.noreply.github.com>
2025-09-08 15:01:38 +0300

    Update ChooseType.tsx

--- src/pages/type/ChooseType.tsx
+++ src/pages/type/ChooseType.tsx
@@ -20,42 +20,7 @@ const ChooseType = () => {
       <div className={styles.innerBody}>
         {t("choose")}
         <div className={flagEnabled ? styles.verticalBtns : styles.btns}>
-          <Button
-            name={t("general")}
-            variant="primary"
-            onClick={() => {
-              localStorage.setItem("topic", "general");
-              setTopic("general");
-              navigate(GAME_ROUTE);
-            }}
-          />
-          <Button
-            name={t("food")}
-            variant="primary"
-            onClick={() => {
-              localStorage.setItem("topic", "food");
-              setTopic("food");
-              navigate(GAME_ROUTE);
-            }}
-          />
-          <Button
-            name={t("movies")}
-            variant="primary"
-            onClick={() => {
-              localStorage.setItem("topic", "movies");
-              setTopic("movies");
-              navigate(GAME_ROUTE);
-            }}
-          />
-          <Button
-            name={t("countries")}
-            variant="primary"
-            onClick={() => {
-              localStorage.setItem("topic", "countries");
-              setTopic("countries");
-              navigate(GAME_ROUTE);
-            }}
-          />
+       
         </div>
       </div>
       <Footer />
